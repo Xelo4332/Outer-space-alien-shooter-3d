b41b7108157a9e85ce1b55f6b837d27c6a1e25be
Xelo4332 <mansor-deni.mamergov@elev.ga.lbs.se>
2024-02-09 15:12:45 +0100

    AudioManager

---
+++ Assets/Script/AudioManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioSource musicSource;
+    public AudioSource soundEffectSource;
+    public static AudioManager instance = null;
+    private AudioClip _currentSoundEffect;
+    public AudioClip CurrentSoundEffct => _currentSoundEffect;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    public void PlaySoundEffect(AudioClip clip)
+    {
+        soundEffectSource.clip = clip;
+        soundEffectSource.Play();
+        if (_currentSoundEffect != clip)
+        {
+            _currentSoundEffect = clip;
+        }
+    }
+
+    public void ClearSoundEffect()
+    {
+        soundEffectSource.Stop();
+        soundEffectSource.clip = null;
+        _currentSoundEffect = null;
+    }
+}
